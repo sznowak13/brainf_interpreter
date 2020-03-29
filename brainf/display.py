0cd874a84e9d1c3d87edc40796d669a73c51e439
Szymon Nowak <snowak@biinsight.pl>
2020-03-29 16:10:29 +0200

    Basic refactor, put some logic into funcs

--- brainf/display.py
+++ brainf/display.py
@@ -13,8 +13,36 @@ class InterpreterDisplay:
         cmd = input(f'[In {exec_line}]: ')
         return cmd
 
-    def _print(self, text, line_break=False):
-        print(text, end='' if not line_break else '\n')
+    def _normalize_left(self, ptr_on):
+        buffer = 0
+        _range = ptr_on - 4
+        if _range <= 0:
+            _range = 1
+        if _range > 1:
+            buffer = _range - 1
+        return _range, buffer
+
+    def _normalize_right(self, ptr_on, table_size):
+        buffer = 0
+        _range = ptr_on + 4
+        if _range >= table_size - 1:
+            _range = table_size - 2
+        if _range < table_size - 2:
+            buffer = _range + 1
+        return _range, buffer
+
+    def _get_buffer_cells(self, low, high):
+        buffer_index_str = f"{low}-{high}"
+        buffer_str = f"{'...'.center(len(buffer_index_str) + 2, ' ')}|"
+        buffer_index_str = buffer_index_str.center(len(buffer_str) - 1, ' ')
+        return buffer_index_str, buffer_str
+
+    def _get_index_and_pov_strs(self, table, cell_no):
+        pov_str = f"{table[cell_no]}"
+        index_str = f"{cell_no}".center(len(pov_str), ' ')
+        pov_str = pov_str.center(len(index_str) + 2, ' ') + '|'
+        index_str = index_str.center(len(pov_str), ' ')
+        return pov_str, index_str
 
     def print_welcome(self):
         print("\n  ** Welcome!")
@@ -24,47 +52,34 @@ class InterpreterDisplay:
 
     def print_brainf_table(self, bf_i):
         # TODO refactor xD
-        left_buffer = 0
-        right_buffer = 0
         pointer_on = bf_i.index
-        left_range = pointer_on - 4
-        if left_range <= 0:
-            left_range = 1
-        if left_range > 1:
-            left_buffer = left_range - 1
-        right_range = pointer_on + 4
-        if right_range >= bf_i.size - 1:
-            right_range = bf_i.size - 2
-        if right_range < bf_i.size - 2:
-            right_buffer = right_range + 1
+        left_range, left_buffer = self._normalize_left(pointer_on)
+        right_range, right_buffer = self._normalize_right(pointer_on, bf_i.size)
         table_str = f"[ {bf_i.table[0]} |"
         index_str = "0".center(len(table_str), ' ')
         ptr_str = '^'.center(len(table_str), ' ')
         if left_buffer:
-            buffer_index_str = f"1-{left_range - 1}"
-            buffer_str = f"{'...'.center(len(buffer_index_str) + 2, ' ')}|"
-            buffer_index_str = f"1-{left_range - 1}".center(len(buffer_str), ' ')
+            buffer_index_str, buffer_str = self._get_buffer_cells(1, left_range - 1)
             index_str += buffer_index_str
             table_str += buffer_str
         for i in range(left_range, right_range + 1):
-            pov_str = f" {bf_i.table[i]} |"
-            index_str += f"{i}".center(len(pov_str), ' ')
+            pov_str, curr_index_str = self._get_index_and_pov_strs(bf_i.table, i)
+            index_str += curr_index_str
             if pointer_on == i:
                 ptr_str = f"{' ' * len(table_str)}{'^'.center(len(pov_str))}"
             table_str += pov_str
         if right_buffer:
-            buffer_index_str = f"{right_range + 1}-{bf_i.size - 2}"
-            buffer_str = f"{'...'.center(len(buffer_index_str) + 2, ' ')}|"
-            buffer_index_str = f"{right_range + 1}-{bf_i.size - 2}".center(len(buffer_str), ' ')
+            buffer_index_str, buffer_str = self._get_buffer_cells(right_range + 1, bf_i.size - 2)
             index_str += buffer_index_str
             table_str += buffer_str
-        end_str = f" {bf_i.table[-1]} ]"
-        index_str += f"{bf_i.size - 1}".center(len(end_str), ' ')
+        end_str = f"{bf_i.table[-1]}"
+        end_index_str = f"{bf_i.size - 1}".center(len(end_str), ' ')
+        end_str = end_str.center(len(end_index_str), ' ') + "]"
+        end_index_str = end_index_str.center(len(end_str) + 1, ' ')
+        index_str += end_index_str
         if pointer_on == bf_i.size - 1:
             ptr_str = f"{' ' * len(table_str)}{'^'.center(len(end_str))}"
         table_str += end_str
-        print()
-        print(f"\tState: {table_str}")
+        print(f"\n\tState: {table_str}")
         print(f"\tIndex: {index_str}")
-        print(f"\tPoint: {ptr_str}")
-        print()
+        print(f"\tPoint: {ptr_str}\n")
